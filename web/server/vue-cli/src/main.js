a9066538318a106f6b5f386126400cb6b36cbaeb
Maksud Bagirov <maksudbagirov@MacBook-Air-Maksud.local>
2025-05-26 15:34:18 +0200

    Updated main.js

--- web/server/vue-cli/src/main.js
+++ web/server/vue-cli/src/main.js
@@ -1,12 +1,17 @@
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
+import "splitpanes/dist/splitpanes.css";
+
+if (!Error.captureStackTrace) {
+  Error.captureStackTrace = () => {};
+}
+
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 import { createVuetify } from "vuetify";
-import "vuetify/styles";
-import "@mdi/font/css/materialdesignicons.css";
-
 const vuetify = createVuetify();
 
 const app = createApp(App);
